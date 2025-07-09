b1fe4c3d4d3a4da6a37d565f0f2752a9059b483a
Luna Ribeiro Alves dos Santos <luna.as@itaipuparquetec.org.br>
2025-07-09 15:04:48 -0300

    init: initial project structure

---
+++ src/test/java/backend/todolist_atletica/BackendTodolistAtleticaApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.todolist_atletica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodolistAtleticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
